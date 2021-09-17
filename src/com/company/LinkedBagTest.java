c8071fb823756c4f4f4d49677a2ca4c17ace1620
Nick Karle <nakarle@cpp.edu>
2021-09-17 16:25:19 -0700

    1. Wrote ResizableArrayBag.toString() and rewrote displayBag() using it; 2. Added a check to doubleCapacity() to set size to 1 if bag.length == 0; Wrote ResizableArrayBag.intersection, union, and difference; Wrote test method for the aforementioned methods.

---
+++ src/com/company/LinkedBagTest.java
@@ -0,0 +1,238 @@
+// package com.company;
+
+import java.lang.reflect.Array;
+
+public class LinkedBagTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Creating an empty bag.");
+        BagInterface<String> aBag = new LinkedBag<>();
+        testIsEmpty(aBag, true);
+        displayBag(aBag);
+
+        String[] contentsOfBag = {"A", "D", "B", "A", "C", "A", "D"};
+        testAdd(aBag, contentsOfBag);
+        testIsEmpty(aBag, false);
+
+        String[] testString = {"A", "D", "Z"};
+        String[] testString2 = {"B", "C", "Z"};
+        testFrequency(aBag, testString);
+        testFrequency(aBag, testString2);
+        testContains(aBag, testString);
+        testContains(aBag, testString2);
+
+        testRemove(aBag);
+
+        //Removing specific strings
+        String[] removeString = {"", "A", "C"};
+        testRemoveSpecific(aBag, removeString);
+
+        //Testing the intersection method
+        System.out.println("\n---------------------------------------------------------------------");
+        System.out.println("Testing intersection bag");
+        String[] bag1 = {"A", "B", "C", "B"};
+        String[] bag2 = {"B", "B", "D", "E"};
+        BagInterface<String> leftBag = new LinkedBag<>();
+        BagInterface<String> rightBag = new LinkedBag<>();
+        testAdd(leftBag, bag1);
+        testAdd(rightBag, bag2);
+        testIntersection(leftBag, rightBag);
+
+        //Testing the union method
+        System.out.println("\n---------------------------------------------------------------------");
+        System.out.println("Testing union bag");
+        String[] unionBag = {"A", "B", "C", "B"};
+        String[] unionBag1 = {"Z", "B", "D", "E"};
+        BagInterface<String> leftBag1 = new LinkedBag<>();
+        BagInterface<String> rightBag1 = new LinkedBag<>();
+        testAdd(leftBag1, unionBag);
+        testAdd(rightBag1, unionBag1);
+        testUnion(leftBag1, rightBag1);
+
+        //Testing the difference method
+        System.out.println("\n---------------------------------------------------------------------");
+        System.out.println("Testing difference bag");
+        String[] differenceBag = {"A", "B", "C", "D", "Z"};
+        String[] differenceBag1 = {"B", "B", "D", "E"};
+        BagInterface<String> leftBag2 = new LinkedBag<>();
+        BagInterface<String> rightBag2 = new LinkedBag<>();
+        testAdd(leftBag2, differenceBag);
+        testAdd(rightBag2, differenceBag1);
+        testDifference(leftBag2, rightBag2);
+//        testDifference(rightBag2, leftBag2);
+
+    } // end main
+
+    // Tests the method isEmpty.
+    // Precondition: If the bag is empty, the parameter empty should be true;
+    // otherwise, it should be false.
+    private static void testIsEmpty(BagInterface<String> bag, boolean empty)
+    {
+        System.out.print("\nTesting isEmpty with ");
+        if (empty)
+            System.out.println("an empty bag:");
+        else
+            System.out.println("a bag that is not empty:");
+
+        System.out.print("isEmpty finds the bag ");
+        if (empty && bag.isEmpty())
+            System.out.println("empty: OK.");
+        else if (empty)
+            System.out.println("not empty, but it is: ERROR.");
+        else if (!empty && bag.isEmpty())
+            System.out.println("empty, but it is not empty: ERROR.");
+        else
+            System.out.println("not empty: OK.");
+    } // end testIsEmpty
+
+    // Tests the method add.
+    private static void testAdd(BagInterface<String> aBag, String[] content)
+    {
+        System.out.print("Adding the following strings to the bag: ");
+        for (int index = 0; index < content.length; index++)
+        {
+            if (aBag.add(content[index]))
+                System.out.print(content[index] + " ");
+            else
+                System.out.print("\nUnable to add " + content[index] +
+                        " to the bag.");
+        } // end for
+        System.out.println();
+
+        displayBag(aBag);
+    } // end testAdd
+
+    // Tests the method toArray while displaying the bag.
+    private static void displayBag(BagInterface<String> aBag)
+    {
+        System.out.println("\nThe bag contains the following string(s):");
+        Object[] bagArray = aBag.toArray();
+        for (int index = 0; index < bagArray.length; index++)
+        {
+            System.out.print(bagArray[index] + " ");
+        } // end for
+
+        System.out.println();
+    } // end displayBag
+
+    //For displaying the bag without a new line character
+    private static void displayBagModify(BagInterface<String> aBag)
+    {
+        System.out.println("The bag contains the following string(s):");
+        Object[] bagArray = aBag.toArray();
+        for (int index = 0; index < bagArray.length; index++)
+        {
+            System.out.print(bagArray[index] + " ");
+        } // end for
+
+        System.out.println();
+    } // end displayBag
+
+    //Testing the method of getting the frequency of a certain object type.
+    private static void testFrequency(BagInterface<String> aBag, String[] content)
+    {
+        System.out.println("\nTesting the method getFrequencyof: ");
+        for(int i = 0; i< content.length; i++)
+        {
+            System.out.println("The count of " + content[i] + " : " + aBag.getFrequencyOf(content[i]));
+        }
+    }
+
+    //Testing the method if it contains a certain entry
+    private static void testContains(BagInterface<String> aBag, String[] content)
+    {
+        System.out.println("\nTesting the method contains: ");
+        for(int i = 0; i< content.length; i++)
+        {
+            System.out.println("Does the bag contain " + content[i] + " ? " + aBag.contains(content[i]));
+        }
+    }
+
+    //Tests the method of removing an unspecified entry
+    private static void testRemove(BagInterface<String> aBag)
+    {
+        System.out.println("\nTesting the method remove()");
+        System.out.println("The number of entries in the bag is: " + aBag.getCurrentSize());
+        aBag.remove();
+        System.out.println("The new number of entries is: " + aBag.getCurrentSize());
+    }
+
+    // Tests the method of removing a specific entry
+    private static void testRemoveSpecific(BagInterface<String> aBag, String[] content)
+    {
+        displayBag(aBag);
+        for(int i = 0; i < content.length; i++)
+        {
+            if(content[i].equals("") || content[i] == null)
+            {
+                System.out.println("Removing a string from the bag");
+                String removedString = aBag.remove();
+                System.out.println("remove() returns " + removedString);
+                displayBag(aBag);
+            }
+            System.out.println("The bag contains " + aBag.getCurrentSize() + " strings");
+            System.out.println("Removing " + content[i] + " from the bag: ");
+            boolean result = aBag.remove(content[i]);
+            System.out.println("The bag now contains " + aBag.getCurrentSize() + " strings");
+            displayBag(aBag);
+        }
+    }
+
+    //Testing the intersection method in the BagInterface for a LinkedBag
+    private static void testIntersection(BagInterface<String> aBag, BagInterface<String> bBag)
+    {
+
+        BagInterface<String> intersectionBag = aBag.intersection(bBag);
+        displayBagModify(intersectionBag);
+        System.out.println("Displaying the same bags to check if there is any change");
+        displayBagModify(aBag);
+        displayBagModify(bBag);
+    }
+
+    //Testing the union method in the BagInterface for a LinkedBag
+    private static void testUnion(BagInterface<String> aBag, BagInterface<String> bBag)
+    {
+
+        BagInterface<String> unionBag = aBag.union(bBag);
+        displayBagModify(unionBag);
+        System.out.println("Displaying the same bags to see if there are any changes made to the original bag");
+        displayBagModify(aBag);
+        displayBagModify(bBag);
+    }
+
+    //Testing the difference method in the BagInterface for a LinkedBag
+    private static void testDifference(BagInterface<String> aBag, BagInterface<String> bBag)
+    {
+        System.out.println("The method: bag1.difference(bag2) is being executed ");
+        BagInterface<String> differenceBag = aBag.difference(bBag);
+        displayBagModify(differenceBag);
+
+        System.out.println("\nThe method: bag2.difference(bag1) is being executed");
+        BagInterface<String> differenceBag1 = bBag.difference(aBag);
+        displayBagModify(differenceBag1);
+
+        System.out.println("\nDisplaying the same bags to check if there are any changes made to the original bag");
+        displayBagModify(aBag);
+        displayBagModify(bBag);
+    }
+
+} // end LinkedBagDemo1
+
+/*
+ Creating an empty bag.
+
+ Testing isEmpty with an empty bag:
+ isEmpty finds the bag empty: OK.
+ The bag contains the following string(s):
+
+ Adding the following strings to the bag: A D B A C A D
+ The bag contains the following string(s):
+ D A C A B D A
+
+ Testing isEmpty with a bag that is not empty:
+ isEmpty finds the bag not empty: OK.
+ */
+
+
+
---
+++ src/com/company/ResizableArrayBag.java
@@ -0,0 +1,361 @@
+package com.company;
+
+import java.util.Arrays;
+/**
+ A class that implements a bag of objects by using an array.
+ The bag is never full.
+ @author Frank M. Carrano, Timothy M. Henry
+ @version 5.0
+ */
+public final class ResizableArrayBag<T> implements BagInterface<T>
+{
+    private T[] bag; // Cannot be final due to doubling
+    private int numberOfEntries;
+    private boolean integrityOK = false;
+    private static final int DEFAULT_CAPACITY = 25; // Initial capacity of bag
+    private static final int MAX_CAPACITY = 10000;
+
+    /** Creates an empty bag whose initial capacity is 25. */
+    public ResizableArrayBag()
+    {
+        this(DEFAULT_CAPACITY);
+    } // end default constructor
+
+    /** Creates an empty bag having a given initial capacity.
+     @param initialCapacity  The integer capacity desired. */
+    public ResizableArrayBag(int initialCapacity)
+    {
+        checkCapacity(initialCapacity);
+
+        // The cast is safe because the new array contains null entries
+        @SuppressWarnings("unchecked")
+        T[] tempBag = (T[])new Object[initialCapacity]; // Unchecked cast
+        bag = tempBag;
+        numberOfEntries = 0;
+        integrityOK = true;
+    } // end constructor
+
+    /** Creates a bag containing given entries.
+     @param contents  An array of objects. */
+    public ResizableArrayBag(T[] contents)
+    {
+        checkCapacity(contents.length);
+        bag = Arrays.copyOf(contents, contents.length);
+        numberOfEntries = contents.length;
+        integrityOK = true;
+    } // end constructor
+
+    /** Adds a new entry to this bag.
+     @param newEntry  The object to be added as a new entry.
+     @return  True. */
+    public boolean add(T newEntry)
+    {
+        checkintegrity();
+        if (isArrayFull())
+        {
+            doubleCapacity();
+        } // end if
+
+        bag[numberOfEntries] = newEntry;
+        numberOfEntries++;
+
+        return true;
+    } // end add
+
+    /** Retrieves all entries that are in this bag.
+     @return  A newly allocated array of all the entries in this bag. */
+    public T[] toArray()
+    {
+        checkintegrity();
+
+        // The cast is safe because the new array contains null entries.
+        @SuppressWarnings("unchecked")
+        T[] result = (T[])new Object[numberOfEntries]; // Unchecked cast
+        for (int index = 0; index < numberOfEntries; index++)
+        {
+            result[index] = bag[index];
+        } // end for
+
+        return result;
+    } // end toArray
+
+    /** Sees whether this bag is empty.
+     @return  True if this bag is empty, or false if not. */
+    public boolean isEmpty()
+    {
+        return numberOfEntries == 0;
+    } // end isEmpty
+
+    /** Gets the current number of entries in this bag.
+     @return  The integer number of entries currently in this bag. */
+    public int getCurrentSize()
+    {
+        return numberOfEntries;
+    } // end getCurrentSize
+
+    /** Counts the number of times a given entry appears in this bag.
+     @param anEntry  The entry to be counted.
+     @return  The number of times anEntry appears in this ba. */
+    public int getFrequencyOf(T anEntry)
+    {
+        checkintegrity();
+        int counter = 0;
+
+        for (int index = 0; index < numberOfEntries; index++)
+        {
+            if (anEntry.equals(bag[index]))
+            {
+                counter++;
+            } // end if
+        } // end for
+
+        return counter;
+    } // end getFrequencyOf
+
+    /** Tests whether this bag contains a given entry.
+     @param anEntry  The entry to locate.
+     @return  True if this bag contains anEntry, or false otherwise. */
+    public boolean contains(T anEntry)
+    {
+        checkintegrity();
+        return getIndexOf(anEntry) > -1; // or >= 0
+    } // end contains
+
+    /** Removes all entries from this bag. */
+    public void clear()
+    {
+        while (!isEmpty())
+            remove();
+    } // end clear
+
+    /** Removes one unspecified entry from this bag, if possible.
+     @return  Either the removed entry, if the removal
+     was successful, or null. */
+    public T remove()
+    {
+        checkintegrity();
+        T result = removeEntry(numberOfEntries - 1);
+        return result;
+    } // end remove
+
+    /** Removes one occurrence of a given entry from this bag.
+     @param anEntry  The entry to be removed.
+     @return  True if the removal was successful, or false if not. */
+    public boolean remove(T anEntry)
+    {
+        checkintegrity();
+        int index = getIndexOf(anEntry);
+        T result = removeEntry(index);
+        return anEntry.equals(result);
+    } // end remove
+
+    // Locates a given entry within the array bag.
+    // Returns the index of the entry, if located,
+    // or -1 otherwise.
+    // Precondition: checkintegrity has been called.
+    private int getIndexOf(T anEntry)
+    {
+        int where = -1;
+        boolean found = false;
+        int index = 0;
+
+        while (!found && (index < numberOfEntries))
+        {
+            if (anEntry.equals(bag[index]))
+            {
+                found = true;
+                where = index;
+            } // end if
+            index++;
+        } // end while
+
+        // Assertion: If where > -1, anEntry is in the array bag, and it
+        // equals bag[where]; otherwise, anEntry is not in the array.
+
+        return where;
+    } // end getIndexOf
+
+    // Removes and returns the entry at a given index within the array.
+    // If no such entry exists, returns null.
+    // Precondition: 0 <= givenIndex < numberOfEntries.
+    // Precondition: checkintegrity has been called.
+    private T removeEntry(int givenIndex)
+    {
+        T result = null;
+
+        if (!isEmpty() && (givenIndex >= 0))
+        {
+            result = bag[givenIndex];          // Entry to remove
+            int lastIndex = numberOfEntries - 1;
+            bag[givenIndex] = bag[lastIndex];  // Replace entry to remove with last entry
+            bag[lastIndex] = null;             // Remove reference to last entry
+            numberOfEntries--;
+        } // end if
+
+        return result;
+    } // end removeEntry
+
+    // Returns true if the array bag is full, or false if not.
+    private boolean isArrayFull()
+    {
+        return numberOfEntries >= bag.length;
+    } // end isArrayFull
+
+    // Doubles the size of the array bag.
+    // Precondition: checkInitialization has been called.
+    private void doubleCapacity()
+    {
+    	int newLength;
+    	if (bag.length == 0) {
+    		newLength = 1;
+    	} else {
+    		newLength = 2 * bag.length;
+    	}
+        
+        checkCapacity(newLength);
+        bag = Arrays.copyOf(bag, newLength);
+    } // end doubleCapacity
+
+    // Throws an exception if the client requests a capacity that is too large.
+    private void checkCapacity(int capacity)
+    {
+        if (capacity > MAX_CAPACITY)
+            throw new IllegalStateException("Attempt to create a bag whose capacity exceeds " +
+                    "allowed maximum of " + MAX_CAPACITY);
+    } // end checkCapacity
+
+    // Throws an exception if receiving object is not initialized.
+    private void checkintegrity()
+    {
+        if (!integrityOK)
+            throw new SecurityException ("ArrayBag object is corrupt.");
+    } // end checkintegrity
+    
+    public String toString() 
+    {
+    	String str = "[";
+    	for (int i = 0; i < numberOfEntries; i++)
+    		str += bag[i] + ",";
+    	if (numberOfEntries != 0)
+    		str = str.substring(0, str.length() - 1);
+    	str += "]";
+    	
+    	return str;
+    } //end toString
+
+    public BagInterface<T> intersection(BagInterface<T> otherBag)
+    {
+    	int maxCap = 0;
+    	if (otherBag.getCurrentSize() > getCurrentSize()) {
+    		maxCap = otherBag.getCurrentSize();
+    	} else {
+    		getCurrentSize();
+    	} //end if
+    	BagInterface<T> newBag = new ResizableArrayBag(maxCap);
+    	
+    	int frequencyOfThisEntry = 0;
+    	for (int i = 0; i < numberOfEntries; i++) {
+    		if (otherBag.contains(bag[i])) {
+    			if (otherBag.getFrequencyOf(bag[i]) < getFrequencyOf(bag[i])) {
+    				//otherBag's frequency is used
+    				frequencyOfThisEntry = otherBag.getFrequencyOf(bag[i]);
+    			} else {
+    				//this bag's frequency is used
+    				frequencyOfThisEntry = getFrequencyOf(bag[i]);
+    			}
+    		} //end if
+    		for (int k = 0; k < frequencyOfThisEntry; k++) {
+    			newBag.add(bag[i]);
+    		} //end for
+    	} //end for
+        return newBag;
+    } //end intersection
+
+    public BagInterface<T> union(BagInterface<T> otherBag)
+    {
+    	BagInterface<T> newBag = new ResizableArrayBag(otherBag.getCurrentSize() + getCurrentSize());
+    	
+    	for (int i = 0; i < getCurrentSize(); i++) {
+    		newBag.add(bag[i]);
+    	} //end for
+    	
+    	
+    	for (int i = 0; i < otherBag.getCurrentSize(); i++) {
+    		newBag.add(otherBag.toArray()[i]);
+    	} //end for
+    	
+        return newBag;
+    }//union
+
+    public BagInterface<T> difference(BagInterface<T> otherBag)
+    {
+    	BagInterface<T> newBag = this;
+    	
+    	for (int i = 0; i < getCurrentSize(); i++) {
+    		newBag.remove(otherBag.toArray()[i]);
+    	} //end for
+    	
+        return newBag;
+    }
+} // end ResizableArrayBag
+
+/*
+ Testing isEmpty with an empty bag:
+ isEmpty finds the bag empty: OK.
+
+ Adding to the bag more strings than its initial capacity.
+ Adding to the bag: A D B A C A D
+ The bag contains 7 string(s), as follows:
+ A D B A C A D
+ Testing isEmpty with a bag that is not empty:
+ isEmpty finds the bag not empty: OK.
+
+
+ Testing the method getFrequencyOf:
+ In this bag, the count of A is 3
+ In this bag, the count of B is 1
+ In this bag, the count of C is 1
+ In this bag, the count of D is 2
+ In this bag, the count of Z is 0
+
+ Testing the method contains:
+ Does this bag contain A? true
+ Does this bag contain B? true
+ Does this bag contain C? true
+ Does this bag contain D? true
+ Does this bag contain Z? false
+
+ Removing a string from the bag:
+ remove() returns D
+ The bag contains 6 string(s), as follows:
+ A D B A C A
+
+ Removing "B" from the bag:
+ remove("B") returns true
+ The bag contains 5 string(s), as follows:
+ A D A A C
+
+ Removing "A" from the bag:
+ remove("A") returns true
+ The bag contains 4 string(s), as follows:
+ C D A A
+
+ Removing "C" from the bag:
+ remove("C") returns true
+ The bag contains 3 string(s), as follows:
+ A D A
+
+ Removing "Z" from the bag:
+ remove("Z") returns false
+ The bag contains 3 string(s), as follows:
+ A D A
+
+ Clearing the bag:
+ Testing isEmpty with an empty bag:
+ isEmpty finds the bag empty: OK.
+
+ The bag contains 0 string(s), as follows:
+ */
+
+
+
